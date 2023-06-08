3a36fbba44d60a6c24005dda2e87c68f07030b19
Bradley Pregon <bradley.pregon@gmail.com>
2023-06-07 23:35:57 -0500

    not really sure what i did

--- radbot/commands/play.js
+++ radbot/commands/play.js
@@ -5,10 +5,13 @@ module.exports = {
 
 	async execute(message, args, client, guildQueue) {
 		console.log("playing cmd: " + args);
-
 		const voiceChannel = message.member.voice.channel;
-		let queue = client.player.createQueue(message.guild.id);
 
+		// Check if queue exists
+		// 	if queue exists, delete it? and then recreate it
+		client.player.deleteQueue(message.guild.id);
+		
+		let queue = client.player.createQueue(message.guild.id);
 		await queue.join(voiceChannel);
 
 		let song = await queue.play(args).catch(err => {
